a9ccb60304c8826b9804545c6b412dd7eaf56836
kittiwake <kittiwake@mail.ru>
2016-01-21 13:43:24 +0300

    получение контента

--- models/Pages.php
+++ models/Pages.php
@@ -8,16 +8,18 @@
 
 class Pages extends BaseModel {
 
-    public function checkAddress($address, $level){
+    public function checkAddress($address, $level, $parent){
 
         $res = $this->dbObject->prepare('
-          SELECT *
-          FROM `router_pages`
-          WHERE `address` = :addr
-          AND `level` = :level
+            SELECT *
+            FROM `router_pages`
+            WHERE `address` = :addr
+            AND `parent_id` = :parr
+            AND `level` = :level
           ');
         $res->execute(array(
             ':addr'=>$address,
+            ':parr'=>$parent,
             ':level'=>$level
         ));
         $res->setFetchMode(PDO::FETCH_ASSOC);
@@ -25,4 +27,20 @@ class Pages extends BaseModel {
         return $res->fetch();
     }
 
+    public function getContent($pageId){
+        $gid = $_SESSION['group'];
+        $res = $this->dbObject->prepare('
+            SELECT `permission`, `content`
+            FROM `router_contents`
+            WHERE `gid` = :gid
+            AND `page_id` = :page_id
+        ');
+        $res->execute(array(
+            ':gid'=>$gid,
+            ':page_id'=>$pageId
+        ));
+        $res->setFetchMode(PDO::FETCH_ASSOC);
+
+        return $res->fetch();
+    }
 } 
