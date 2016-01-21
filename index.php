3f9630bec470ada1e0fe41528aedd2900c8ba986
kittiwake <kittiwake@mail.ru>
2016-01-21 13:43:42 +0300

    получение контента

--- index.php
+++ index.php
@@ -13,8 +13,10 @@ if(!$dbObject) exit('Unable to connect to database');
 //начать сессию
 $sessObject = new SessionController($dbObject);
 $sessError = $sessObject->start();
-//if(!empty($sessError)) exit($sessError);
 
 // Загружаем router
 $router = new RouterController($dbObject);
 $routerError = $router->run();
+
+$content = new ContentController($dbObject);
+$content->display($sessError, $routerError);
