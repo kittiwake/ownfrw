a4cbe72f9ce36c7e5a34b80dcff0b48527153f38
kittiwake <kittiwake@mail.ru>
2016-01-21 13:42:08 +0300

    генератор соли, добавление пользователя

--- models/Users.php
+++ models/Users.php
@@ -10,11 +10,35 @@ class Users extends BaseModel {
 
     public function getUserByLogin($val){
 
-        $res = $this->dbObject->prepare('SELECT * FROM `users` WHERE `user_login` = :val');
+        $res = $this->dbObject->prepare('SELECT * FROM `users` WHERE `login` = :val');
         $res->execute(array(
             ':val'=>$val));
         $res->setFetchMode(PDO::FETCH_ASSOC);
 
         return $res->fetch();
+    }
+
+    public function addUser($log, $pass, $group){
+        $salt = self::generateCode(32);
+        $res = $this->dbObject->prepare("
+INSERT INTO users (login, password, salt, group, registration_date)
+VALUES (:login, :password, :salt, :group, CURDATE())
+");
+        $res -> execute(array(
+            'login' => $log,
+            'password'=> $pass,
+            'salt' => $salt,
+            'group' => $group
+        ));
+    }
+
+    public static function generateCode($length=6) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRQSTUVWXYZ0123456789";
+        $code = "";
+        $clen = strlen($chars) - 1;
+        while (strlen($code) < $length) {
+            $code .= $chars[mt_rand(0,$clen)];
+        }
+        return $code;
     }
 }
