2844c6753de2fbfb6336caef250486ca484e8d4b
kittiwake <kittiwake@mail.ru>
2016-01-21 13:38:41 +0300

    новый

---
+++ controllers/ContentController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: kittiwake
+ * Date: 21.01.2016
+ */
+
+class ContentController {
+
+    private $dbObject;
+
+    public function __construct($dbObj) {
+        $this->dbObject = $dbObj;
+    }
+
+    public function display($sessError, $routerError){
+        switch ($sessError){
+            case 'Invalid login or password' :
+            case 'You are denied access' :
+                ;
+                break;
+            case 'You need to log in' :
+                ;
+                break;
+            case 'session started' :
+            case 'session ok' :
+                $this->buildLoggedInPage($routerError);
+                break;
+        }
+    }
+
+    private function buildLoggedInPage($pageId){
+        $modelContent = new Pages($this->dbObject);
+        $cont = $modelContent->getContent($pageId);
+        if($cont['permission'] == 'open'){
+            return $cont['content'];
+        }
+
+    }
+} 
